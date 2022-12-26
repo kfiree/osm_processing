cbd077952aa5b4b3a2e4e0b85d0dade23ace799f
kfiree <kfirettinger@gmail.com>
2022-12-27 00:11:37 +0200

    fix maven after splitting

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         String path = "C:\\Users\\kfire\\Development\\projects\\Final Project\\israel.pbf";
         RoadMapHandler.CreateMap(path);
+
     }
 }
